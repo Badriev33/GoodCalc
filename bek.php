9b1c8d82382af18826cfcee1293773c79fd5ba07
Your Name <avasbadriev@mail.ru>
2022-01-08 01:41:11 +0400

    vueandphp

---
+++ bek.php
@@ -0,0 +1,9 @@
+<?php
+   $name = $_POST['name'];
+   
+   if (isset($_POST['name']) && preg_match('#^[a-z\d\s*/+.()_-]+$#si', $_POST['name'])) { //Используем регулярное выражение для калькулятора
+    echo $_POST['name'] . ' = ' . eval('return ' . $_POST['name'] . ';'); //eval-исполнит код находящийся в строке
+} else echo ""; //выводим Errror!! при недопустимых значениях
+   
+   
+   print 'name';
--- calc.js
+++ calc.js
@@ -1,7 +1,67 @@
 var vue = new Vue({
+    el: '#app',
+    data: {
+      result: '',
+      numseven: [7],
+      numeight: [8],
+	  numnine: [9],
+	  opplus: ['+'],
+	  numfour: [4],
+	  numfive: [5],
+	  numsiks: [6],
+	  opminus: ['-'],
+	  numone: [1],
+	  numtwo: [2],
+	  numthree: [3],
+	  opshare: ['/'],
+	  numzero: [0],
+	  dot: ['.'],
+	  opmulti: ['*'],
+    },
+    methods: {
+      input: function(char) {
+        //this.result = this.result.toString();
+        this.result+=char;
+      },
+      
+      calc: function() {
+        
+      }
+    }
+  })
 
-el: '#app',
-data: {
-    result: ""
-}
-})
+
+
+$("#send").on("click", function() {
+	
+	var name = $("#name").val().trim();
+	
+
+	if(name == "") {
+		$("#errorMess").text("Введите число");
+		return false;
+	
+	}
+
+	$("#errorMess").text("");
+
+	$.ajax({
+	  url: 'bek.php',
+	  type: 'POST',
+	  cache: false,
+	  data: { 'name': name },
+	  dataType: 'html',
+	 // beforeSend: function() {
+		//$("#sendMail").prop("disabled", true);
+	  //},
+	  success: function(data) {
+      alert(data);
+	  
+	
+	
+		//$("#send").prop("disabled", false);
+	  }
+	});
+
+	
+});
